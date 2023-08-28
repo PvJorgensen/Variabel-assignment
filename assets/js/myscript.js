9674721dec8fdacf80b97594b2ee9b2178c4f983
Patrik_Jørgensen <vadmand806@gmail.com>
2023-08-28 11:49:43 +0200

    Update myscript.js

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,18 +1,25 @@
+console.group('opgave 1')
 const navn = "Patrik"
 console.log(navn);
+console.groupEnd('opgave 1')
 
+console.group('opgave 2')
 const x = 5
 const y = 2
 const sum = x+y
 console.log(sum);
+console.groupEnd('opgave 2')
 
-
+console.group('opgave 3')
 let alder = 24
 alder += 5
 console.log(alder);
+console.groupEnd('opgave 3')
 
+console.group('opgave 4')
 let myNumber = 1
 myNumber ++
 console.log(myNumber);
 myNumber --
 console.log(myNumber);
+console.groupEnd('opgave 4')
