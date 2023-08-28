987a24c12462525f62495783a30f0e593b38220b
Patrik_Jørgensen <vadmand806@gmail.com>
2023-08-28 11:31:06 +0200

    Update myscript.js

---
+++ assets/js/myscript.js
@@ -0,0 +1,18 @@
+const navn = "Patrik"
+console.log(navn);
+
+const x = 5
+const y = 2
+const sum = x+y
+console.log(sum);
+
+
+let alder = 24
+alder += 5
+console.log(alder);
+
+let myNumber = 1
+myNumber ++
+console.log(myNumber);
+myNumber --
+console.log(myNumber);
